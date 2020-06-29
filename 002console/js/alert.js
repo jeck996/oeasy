151fd6abd2606cfc7bfca3ce0f92a841e97230d0
jeck996 <lvrm03@hotmail.com>
2020-06-29 20:12:23 +0800

    oeasy learning js

---
+++ 002console/js/alert.js
@@ -0,0 +1,11 @@
+alert("你怎么打开了这个网页");
+woDeTaoLu();
+alert("你相信奇迹吗？");
+alert("让我来向你体格问题");
+woDeTaoLu();
+alert("果然没有奇迹")
+alert("我们就这样放弃吗？")
+woDeTaoLu();
+
+
+alert("这是最后一句，88");
---
+++ 002console/js/woDeTaoLu.js
@@ -0,0 +1,7 @@
+function woDeTaoLu() {
+    alert("你猜一会会发生生么");
+    alert("再等等");
+    alert("也许有奇迹");
+}
+
+
